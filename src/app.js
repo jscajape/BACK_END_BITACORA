41c30e70f534669480c1d28b0a4341869f242cd9
Sebastián Cajape <jscajape@espe.edu.ec>
2018-10-10 11:23:10 -0500

    correccion

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ mongoose.connect('mongodb://root:fQf6zgnDKvDD@localhost:27017/Bitacora')//Permit
     .then(db => console.log('BD está conectada'))
     .catch(err => console.error(err));
 */
-mongoose.connect('mongodb://host:0.0.0.0:27017/Bitacora/', {
+mongoose.connect('mongodb://localhost:27017/Bitacora/', {
         user: 'root',
         pass: 'fQf6zgnDKvDD',
       }).then(db => console.log('BD está conectada'))
